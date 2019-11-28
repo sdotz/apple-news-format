bb28b561cd93db97acb9d1ced70e59a5b4857867
Stephen Dotz <scdotz@forbes.com>
2019-11-28 14:13:32 -0500

    omit the text_decoration fields when empty

--- pkg/styles/text_decoration.go
+++ pkg/styles/text_decoration.go
@@ -1,6 +1,6 @@
 package styles
 
 type TextDecoration struct {
-	Color Color `json:"color"`
-	Width int   `json:"width"`
+	Color Color `json:"color,omitempty"`
+	Width int   `json:"width,omitempty"`
 }
